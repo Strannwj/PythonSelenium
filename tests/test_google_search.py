a377e1c4ccd6886e9610382dc8eecbe18b74d68a
e.stepanov <e.stepanov@movavi.com>
2024-10-29 21:31:31 +0300

    fix structures

--- tests/test_google_search.py
+++ tests/test_google_search.py
@@ -1,20 +1,12 @@
-import pytest
-from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+
 GOOGLE = 'https://www.google.com/'
 SEARCH_BOX = (By.NAME, 'q')
 SEARCH_PHRASE = 'Selenium'
-
-
-@pytest.fixture
-def driver():
-    driver = webdriver.Chrome()
-    yield driver
-    driver.quit()
 
 
 def test_google_search(driver):
