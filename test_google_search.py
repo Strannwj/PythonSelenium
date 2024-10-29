d13ac028aec50d3fbd7a85b069eb3b5ad6e6b593
e.stepanov <e.stepanov@movavi.com>
2024-10-29 17:45:07 +0300

    add test_google_search

---
+++ test_google_search.py
@@ -0,0 +1,28 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+GOOGLE = 'https://www.google.com/'
+SEARCH_BOX = (By.NAME, 'q')
+SEARCH_PHRASE = 'Selenium'
+
+
+@pytest.fixture
+def driver():
+    driver = webdriver.Chrome()
+    yield driver
+    driver.quit()
+
+
+def test_google_search(driver):
+    driver.get(GOOGLE)
+    search_box = driver.find_element(*SEARCH_BOX)
+    search_box.send_keys(SEARCH_PHRASE)
+    search_box.send_keys(Keys.ENTER)
+
+    WebDriverWait(driver, 10).until(EC.title_contains("Selenium"))
+
+    assert "Selenium" in driver.title
