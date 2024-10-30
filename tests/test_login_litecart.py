1e793763736cb29b99a1d18c5ef60573badc9d20
e.stepanov <e.stepanov@movavi.com>
2024-10-30 17:52:04 +0300

    add test_login_litecart.py

---
+++ tests/test_login_litecart.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+
+
+def test_login_litecart(driver):
+    driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog")
+    driver.find_element(By.NAME, "username").send_keys("admin")
+    driver.find_element(By.NAME, "password").send_keys("admin")
+    driver.find_element(By.NAME, "login").click()
