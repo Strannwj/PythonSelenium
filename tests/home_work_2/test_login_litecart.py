94e0517c2cfe8cf217de0626d02a44e4a5cd71c1
e.stepanov <e.stepanov@movavi.com>
2024-11-05 21:34:04 +0300

    fix structure

--- tests/home_work_2/test_login_litecart.py
+++ tests/home_work_2/test_login_litecart.py
@@ -1,7 +1,5 @@
 from selenium.webdriver.common.by import By
 
-from conftest import driver
-
 
 def test_login_litecart(driver):
     driver.get("http://localhost/litecart/admin/?app=catalog&doc=catalog")
--- tests/home_work_3/test_google_search_different_browsers.py
+++ tests/home_work_3/test_google_search_different_browsers.py
@@ -3,7 +3,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
-from conftest import driver_fx, driver_eg
+from lib.conftest import driver_fx, driver_eg
 
 
 GOOGLE = 'https://www.google.com/'
